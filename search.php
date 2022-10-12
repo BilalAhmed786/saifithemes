033d5fbc8026d756c3f6a37116c490fd61ca9b96
BilalAhmed786 <fasst.sallar@gmail.com>
2022-10-12 16:40:49 +0500

    BUG-FIXED

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 <div class="content-container">
-    <h1 class="page-title"><?php _e( 'Search results for:', 'nd_dosth' ); ?></h1>
+    <h1 class="page-title"><?php _e( 'Search results for', 'nd_dosth' ); ?></h1>
     <div class="search-query"><?php echo get_search_query(); ?></div>    
     <div class="container">
         <div class="row">
@@ -21,7 +21,7 @@ get_header();
                 <?php endwhile; ?>
                 <?php the_posts_pagination(); ?>
             <?php else: ?>
-                <p><?php _e( 'No Search Results found', 'nd_dosth' ); ?></p>
+                <p class="noresult"><?php _e( 'No Search Results found', 'nd_dosth' ); ?></p>
             <?php endif; ?>
             </div>
             <div id="blog-sidebar" class="col-md-4">
